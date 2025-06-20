b868187a4c123939b34568d672d08ca36507b255
paschyz <paschy2002@gmail.com>
2025-06-20 14:38:19 -0400

    added colors and font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import './App.css'
+/// <reference types="vite-plugin-svgr/client" />
+
+import "./App.css";
+import logo_black from "./assets/logo_pz_black.png";
+import Logo from "./assets/logo.svg?react";
+
+import avatar from "./assets/avatar.png";
 
 function App() {
-
   return (
-    <>
-     My portfolio
-    </>
-  )
+    <div className="">
+      <div className="flex justify-center py-8 ">
+        <Logo className="w-10 h-10 text-secondary" />
+      </div>
+      <div className="flex justify-center border gap-[1vw]">
+        <div className="border">
+          <h1 className=" text-6xl text-secondary font-schabo">
+            Hello, I'm Pascal, a Web Developper.
+          </h1>
+          <p className="font-outfit text-fourth">from Paris, France</p>
+        </div>
+        {/* <div className="border rounded-4xl rounded-tl-none bg-orange-400 h-[60vh] w-[40vw]">
+          <img className="w-xl" src={avatar} alt="avatar" />
+        </div> */}
+        {/* <div className="bg-blue-600 h-"></div> */}
+        <div
+          className="w-[400px] h-[300px] bg-blue-500 "
+          style={{
+            clipPath: "path('M 0 L 10 50 L 100 Z')",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 }
 
-export default App
-
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        // svgr options
+      },
+    }),
+  ],
+});
